98f7e647678909e87cb009f44a884595347c2541
hyjoong <maplesosser@naver.com>
2024-10-20 14:36:58 +0900

    feat: 색상 정의를 위한 color.dart 파일 추가

---
+++ lib/const/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Colors.black;
+const Color secondaryColor = Colors.white;
+const Color lightRed = Color(0xFFF75656);
+const Color darkRed = Color(0xFFFF5757);
+const Color grey = Color(0xFFF1F3F4);
