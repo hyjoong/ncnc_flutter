add536c07a6660055f421f538fbc0026a9ac23b4
hyjoong <maplesosser@naver.com>
2024-10-20 15:02:25 +0900

    feat: CustomAppBar 위젯 구현

---
+++ lib/components/custom_app_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:ncnc_flutter/const/color.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool isHomeScreen;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    required this.isHomeScreen,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: AppBar(
+        leading: IconButton(
+          padding: EdgeInsets.only(left: 16),
+          icon: Icon(isHomeScreen ? Icons.dehaze : Icons.chevron_left),
+          onPressed: isHomeScreen
+              ? () {} /// Home menu button action
+              : () {
+                  Navigator.of(context).pop();
+                },
+        ),
+        centerTitle: true,
+        title: Text(
+          title,
+          style: TextStyle(
+            color: primaryColor,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        backgroundColor: secondaryColor,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(60);
+}
