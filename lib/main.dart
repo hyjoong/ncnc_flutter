8444f4e5fe7652718031cf2791c4051268e435ce
hyjoong <maplesosser@naver.com>
2024-10-25 19:53:36 +0900

    feat: env 및 배경색상 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:ncnc_flutter/const/color.dart';
 import 'package:ncnc_flutter/screen/home_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv .load(fileName: ".env");
   runApp(MaterialApp(
     home: HomeScreen(),
+    theme:ThemeData(scaffoldBackgroundColor: grey)
   ));
 }
