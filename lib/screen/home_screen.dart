87523770b952c49aae80c612b0ef926585c8902a
hyjoong <maplesosser@naver.com>
2024-10-25 19:54:26 +0900

    feat: home screen 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,18 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:ncnc_flutter/components/category_list.dart';
 import 'package:ncnc_flutter/components/custom_app_bar.dart';
+import 'package:ncnc_flutter/components/sale_list.dart';
+import 'package:ncnc_flutter/const/color.dart';
+import 'package:ncnc_flutter/models/category_model.dart';
+import 'package:ncnc_flutter/models/sale_model.dart';
+import 'package:ncnc_flutter/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Category>> categories;
+  late Future<List<SaleItem>> saleItems;
+
+  @override
+  void initState() {
+    super.initState();
+    categories = ApiService.getCategories();
+    saleItems = ApiService.getSaleItems();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
         title: 'Home',
         isHomeScreen: true,
       ),
-      body: Center(
-        child: Text('Welcome to Home Screen'),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          setState(() {
+            categories = ApiService.getCategories();
+            saleItems = ApiService.getSaleItems();
+          });
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder<List<Category>>(
+                future: categories,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return CategoryList(categories: snapshot.data!);
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                },
+              ),
+              const SizedBox(height: 14),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Container(
+                  child: Text(
+                    '놓치지 마세요',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: darkRed,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 4),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  '오늘의 땡처리콘!',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              FutureBuilder<List<SaleItem>>(
+                future: saleItems,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return SaleList(items: snapshot.data!);
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  return const Center(child: CircularProgressIndicator());
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
