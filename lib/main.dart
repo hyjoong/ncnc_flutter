133743dce22eb7080c66fce121b6aac0c0aab498
hyjoong <maplesosser@naver.com>
2024-11-08 16:18:54 +0900

    refactor: 앱 초기화 MyApp 위젯으로 분리

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,18 @@ import 'package:ncnc_flutter/const/color.dart';
 import 'package:ncnc_flutter/screen/home_screen.dart';
 
 void main() async {
-  await dotenv .load(fileName: ".env");
-  runApp(MaterialApp(
-    home: HomeScreen(),
-    theme:ThemeData(scaffoldBackgroundColor: grey)
-  ));
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home:  const HomeScreen(),
+        theme: ThemeData(scaffoldBackgroundColor: grey)
+    );
+  }
 }
